7162da16dd3e687db54e37c68773c89036bb9da1
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-07-01 11:37:19 -0700

    .

--- fadein.js
+++ fadein.js
@@ -1,7 +1,7 @@
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
-    var tags = $("section", "div")
+    var tags = $("section")
 
     for (var i = 0; i < tags.length; i++) {
         var tag = tags[i]
