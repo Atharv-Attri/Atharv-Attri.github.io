3e1fd548bc0ce9ff0f33cc180a00623a6960a32f
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-07-01 12:02:25 -0700

    .

---
+++ run.py
@@ -0,0 +1,8 @@
+from http.server import *
+from http.client import *
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('127.0.0.1', 8100)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+run()
